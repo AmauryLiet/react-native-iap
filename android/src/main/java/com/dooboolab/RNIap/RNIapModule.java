dd751bc78b6a8ba912f86a2b610a0426a0d78ea4
Amaury Liet <amaury.liet@hotmail.fr>
2020-09-18 13:53:34 +0200

    fix/prevent-UnsupportedOperationException-crash

--- android/src/main/java/com/dooboolab/RNIap/RNIapModule.java
+++ android/src/main/java/com/dooboolab/RNIap/RNIapModule.java
@@ -248,7 +248,7 @@ public class RNIapModule extends ReactContextBaseJavaModule implements Purchases
           promise.resolve(false);
           return;
         }
-        final List<Purchase> pendingPurchases = Collections.EMPTY_LIST;
+        final List<Purchase> pendingPurchases = new ArrayList<>();
         for (Purchase purchase : purchases) {
           // we only want to try to consume PENDING items, in order to force cache-refresh for them
           if (purchase.getPurchaseState() == Purchase.PurchaseState.PENDING) {
